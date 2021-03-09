3ee713c85a02fc69ddb98c3025d60ae8d60606ac
Cyber <22157381+cybrnetic@users.noreply.github.com>
2021-03-08 23:39:42 -0800

    Initial Commit
    Express Overview
    Overview of Express framework, going over server setup, API (GET/POST/PUT/DELETE), Routes, Req/Res Objects, Middleware functions, Express Router, Template Engine setup with views (Handlebars), etc.
    
    - setting up the env. tools
    - creating basic Express server
    - Create a Route
    - static folder
    - creating a simple REST API
    - MiddleWare
    - express route
    - Requests (GET/POST/PUT/DELETE)
    - Rendering Templates (express-handlebars)
    
    ## Quick Start
    ```bash
    # Install dependencies
    npm install
    
    # Serve on localhost:5000
    npm run dev
    ```

---
+++ members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'John',
+    email: 'john@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Bob',
+    email: 'bob@gmail.com',
+    status: 'inactive',
+  },
+  {
+    id: 3,
+    name: 'Bill',
+    email: 'bill@gmail.com',
+    status: 'active',
+  },
+];
+
+module.exports = members;
